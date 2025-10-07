9ba1d80e355b54b4099c7d427c0bac837b5b5bd5
Wallace Maia <wallacemaia2007@gmail.com>
2025-10-07 15:00:11 -0300

    Feat: Create components

---
+++ src/app/cadastro/cadastro.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-cadastro',
+  imports: [],
+  templateUrl: './cadastro.html',
+  styleUrl: './cadastro.scss'
+})
+export class Cadastro {
+
+}
